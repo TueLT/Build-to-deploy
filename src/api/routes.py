vanTanIsReal/copy_build_phase

feat(chat): jump-to-unread button + per-row AI toggle; fix(calendar): deterministic scope
Chat - "jump to first unread message" (long conversation with a real backlog):
- chat_service.get_first_unread_message_id() (same unread predicate as
  build_conversation_summary's unread_count, just .order_by(asc()).limit(1));
  GET /conversations/{id}/messages only computes it on the initial page (no
  `before`), not on every scroll-up pagination call
- useMessages.js sizes its initial fetch off the conversation's known
  unread_count so the boundary is actually loaded, not just the default 50
- ChatPage.jsx now calls markRead() only after messages finish loading, not in
  onSelect - removes a real race against the same last_read_at the marker read
- MessageArea.jsx: inline divider + floating jump button; hides itself if the
  target somehow isn't in the loaded window instead of a dead click

Chat - AI permission toggle directly in the conversation list (per screenshot):
- ConversationSummary gains ai_permission_granted, computed in
  build_conversation_summary next to unread_count - no new endpoint
- ConversationList.jsx: per-row Bootstrap switch; row wrapper changed from
  <button> to role="button" div since a checkbox can't nest inside a button
- ChatPage.jsx centralizes the toggle (list, header pill, AI panel all update
  through one function) so all three never disagree with each other

Calendar - fix "tuần này tôi có lịch gì" wrongly answering "không có gì":
  list_calendar_events left the LLM to freehand-compute time_min_iso/
  time_max_iso, which reliably anchored "this week" at the current moment
  instead of the start of the week, silently excluding real events earlier in
  the week. New calendar_service.resolve_scope() computes
  today/this_week/next_7_days/next_30_days deterministically (same reasoning as
  chat_service's MessageScope), exposed as list_calendar_events's new `scope`
  arg; system prompt tells the planner to prefer it over freehand ISO math.

281->2

src/api/routes.py
@@ -6,13 +6,14 @@ from langgraph.types import Command
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.agents import graph as agent_graph
-from src.api.rate_limit import limiter
+from src.api.rate_limit import crud_rate_limit, limiter
 from src.auth.dependencies import get_current_user
 from src.config import get_settings
 from src.db.models import User
 from src.db.session import get_db
 from src.models.schemas import ChatMessage, ChatRequest, ChatResponse, InterruptPayload, ResumeRequest
-from src.services import chat_service, quick_action_service, usage_service
+from src.models.usage_schemas import UsageStatusOut
+from src.services import assistant_thread_service, chat_service, quick_action_service, usage_service
 
 router = APIRouter()
 
@@ -131,12 +132,27 @@ async def chat(
         result = await agent_graph.agent.ainvoke(inputs, config)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    return _build_chat_response(result, thread_id)
+    response = _build_chat_response(result, thread_id)
+
+    if body.conversation_id is None and response.status != "error":
+        # Personal Assistant session (not an AIPanel-embedded call, which always sets
+        # conversation_id) - keep the /assistant page's "Gần đây" thread list in sync.
+        # quick_action requests never reach this point (they return earlier above), so no separate
+        # check for that is needed here.
+        preview = response.response if response.status == "completed" else body.message
+        await assistant_thread_service.touch_new_or_existing(
+            db, thread_id=thread_id, owner_id=current_user.id, user_message=body.message, ai_preview=preview
+        )
+    return response
 
 
 @router.post("/chat/resume", response_model=ChatResponse)
 @limiter.exempt
-async def resume_chat(request: ResumeRequest, current_user: User = Depends(get_current_user)) -> ChatResponse:
+async def resume_chat(
+    request: ResumeRequest,
+    current_user: User = Depends(get_current_user),
+    db: AsyncSession = Depends(get_db),
+) -> ChatResponse:
     """Resume an interrupted agent run with the user's confirm/reject decision.
 
     Exempt from rate limiting (see src/api/rate_limit.py) - same reasoning as its exemption from
@@ -152,7 +168,15 @@ async def resume_chat(request: ResumeRequest, current_user: User = Depends(get_c
         )
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    return _build_chat_response(result, request.thread_id)
+    response = _build_chat_response(result, request.thread_id)
+
+    if response.status != "error":
+        # Only touches a row that already exists (assistant_thread_service.touch_if_exists) - a
+        # resume for a conversation-embedded interrupt (e.g. AIPanel's "Suggest reminder") never
+        # had one created in the first place, and must stay that way.
+        preview = response.response if response.status == "completed" else "Đang chờ xác nhận thêm..."
+        await assistant_thread_service.touch_if_exists(db, thread_id=request.thread_id, ai_preview=preview)
+    return response
 
 
 @router.get("/status")
@@ -160,3 +184,13 @@ async def resume_chat(request: ResumeRequest, current_user: User = Depends(get_c
 async def agent_status():
     """Kiểm tra trạng thái agent."""
     return {"status": "ready", "agent": "LangGraph Agent v1.0"}
+
+
+@router.get("/usage/status", response_model=UsageStatusOut, dependencies=[Depends(crud_rate_limit)])
+async def usage_status(current_user: User = Depends(get_current_user)) -> UsageStatusOut:
+    """Non-admin usage indicator (Sidebar.jsx's "Ngân sách AI hôm nay" widget) - any authenticated
+    user, not just admins (see usage_service.get_usage_summary() for exactly what is/isn't
+    included; the admin-only breakdown with cost/model data stays at GET /admin/stats)."""
+    del current_user  # only used as the auth gate
+    summary = await usage_service.get_usage_summary()
+    return UsageStatusOut(**summary)

TueLT/Build-to-deploy

src/api/routes.py
@@ -17,7 +17,8 @@ from src.models.schemas import (
     InterruptPayload,
     ResumeRequest,
 )
-from src.services import chat_service, consent_service, usage_service
+from src.models.usage_schemas import UsageStatusOut
+from src.services import assistant_thread_service, chat_service, consent_service, usage_service
 from src.services.authorization_service import require_conversation_access
 from src.services.workspace_service import resolve_workspace_for_user
 
@@ -160,7 +161,17 @@ async def chat(
         result = await agent_graph.agent.ainvoke(inputs, config)
     except Exception:
         raise HTTPException(status_code=500, detail="AI service is temporarily unavailable")
-    return _build_chat_response(result, thread_id, context_scope)
+    response = _build_chat_response(result, thread_id, context_scope)
+    if request.conversation_id is None and response.status != "error":
+        preview = response.response if response.status == "completed" else request.message
+        await assistant_thread_service.touch_new_or_existing(
+            db,
+            thread_id=thread_id,
+            owner_id=current_user.id,
+            user_message=request.message,
+            ai_preview=preview,
+        )
+    return response
 
 
 @router.post("/chat/resume", response_model=ChatResponse)
@@ -194,10 +205,25 @@ async def resume_chat(
         )
     except Exception:
         raise HTTPException(status_code=500, detail="AI service is temporarily unavailable")
-    return _build_chat_response(result, request.thread_id)
+    response = _build_chat_response(result, request.thread_id)
+    if response.status != "error":
+        preview = response.response if response.status == "completed" else "Đang chờ xác nhận thêm..."
+        await assistant_thread_service.touch_if_exists(
+            db,
+            owner_id=current_user.id,
+            thread_id=request.thread_id,
+            ai_preview=preview,
+        )
+    return response
 
 
 @router.get("/status")
 async def agent_status():
     """Kiểm tra trạng thái agent."""
     return {"status": "ready", "agent": "LangGraph Agent v1.0"}
+
+
+@router.get("/usage/status", response_model=UsageStatusOut)
+async def usage_status(current_user: User = Depends(get_current_user)) -> UsageStatusOut:
+    del current_user
+    return UsageStatusOut(**(await usage_service.get_usage_summary()))